b98498bf7b4e2a4050ec4a2a441259949f7873e1
Justin Majam <pmajam@my.bcit.ca>
2021-04-03 11:20:58 -0700

    made signup functionality for signUp.js

--- front_end/js/signUp.js
+++ front_end/js/signUp.js
@@ -1,3 +1,32 @@
 document.getElementById("back").addEventListener("click", () => {
     window.location.href = "bloodPressure.html";
 });
+
+document.getElementById("signUpBtn").addEventListener("click", () => {
+    let xttp = new XMLHttpRequest();
+    
+    document.getElementById("signUpBtn").addEventListener("click", async () => {
+        xttp.open("POST", "https:littlefatlamb.com/4537/termproject/API/V1/user/signup/", true);
+        xttp.setRequestHeader("Content-Type", "application/json");
+        let data = getInfo();
+        if (data.username === "" || data.username === "") {
+            window.alert("400: invalid password or username!");
+        } else {
+            xttp.send(JSON.stringify(data));
+        }
+
+        xttp.onreadystatechange = () => {
+            if (this.readyState === 4 && this.status === 202) {
+                console.log(xttp.responseText);
+            }
+        }
+    })
+});
+
+getInfo = () => {
+    let username = document.getElementById("user");
+    let password = document.getElementById("pw");
+
+    return {"username": username.value, "password": password.value};
+};
+
