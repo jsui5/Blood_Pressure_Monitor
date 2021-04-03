e4c19458dd8de0cddf42badb6dc1e84d31b11bf1
Justin Majam <pmajam@my.bcit.ca>
2021-04-03 12:04:52 -0700

    edited signUp.js

--- front_end/js/signUp.js
+++ front_end/js/signUp.js
@@ -2,28 +2,30 @@ document.getElementById("back").addEventListener("click", () => {
     window.location.href = "bloodPressure.html";
 });
 
-document.getElementById("signUpBtn").addEventListener("click", () => {
+document.getElementById("signUpBtn").addEventListener("click", async () => {
+    console.log("trying to signUp");
     let xttp = new XMLHttpRequest();
+    xttp.open("POST", "https://littlefatlamb.com/4537/termproject/API/V1/user/signup", true);
+    xttp.setRequestHeader("Content-Type", "application/json");
+    let data = getInfo();
+    if (data.username === "" || data.username === "") {
+        window.alert("400: invalid password or username!");
+    } else {
+        console.log("SENDING");
+        console.log(JSON.stringify(data));
+        xttp.send(JSON.stringify(data));
+    }
 
-    document.getElementById("signUpBtn").addEventListener("click", async () => {
-        xttp.open("POST", "https:littlefatlamb.com/4537/termproject/API/V1/user/signup/", true);
-        xttp.setRequestHeader("Content-Type", "application/json");
-        let data = getInfo();
-        if (data.username === "" || data.username === "") {
-            window.alert("400: invalid password or username!");
-        } else {
-            xttp.send(JSON.stringify(data));
+    xttp.onreadystatechange = () => {
+        console.log("processing");
+        if (this.readyState === 4 && this.status === 202) {
+            console.log(xttp.responseText);
+            //localStorage.setItem("username", data.username);
+            window.location.href = "detail.html";
         }
-
-        xttp.onreadystatechange = () => {
-            if (this.readyState === 4 && this.status === 202) {
-                console.log(xttp.responseText);
-                //localStorage.setItem("username", data.username);
-                window.location.href = "detail.html";
-            }
-        }
-    })
+    }
 });
+
 
 getInfo = () => {
     let username = document.getElementById("user");
