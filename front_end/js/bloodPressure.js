7a626b774d064ff55208bbdd30eabec741b7c243
Jay Sui <jsui5@my.bcit.ca>
2021-04-02 17:14:24 -0700

    Init Commit

---
+++ front_end/js/bloodPressure.js
@@ -0,0 +1,4 @@
+document.getElementById("back").addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+
---
+++ front_end/js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("doctor").addEventListener("click", () => {
+    window.location.href = "bloodPressure.html"
+});
+
+document.getElementById("patient").addEventListener("click",  () => {
+    window.location.href = "advice.html"
+});
+
+document.getElementById("admin").addEventListener("click",  () => {
+    window.location.href = "admin.html"
+});
