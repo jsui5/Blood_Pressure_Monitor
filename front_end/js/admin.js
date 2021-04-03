ca5848b7495c16e1182a579c4d866e370b570e8f
Justin Majam <pmajam@my.bcit.ca>
2021-04-02 20:55:16 -0700

    made functionality for getting endpoint stats

--- front_end/js/admin.js
+++ front_end/js/admin.js
@@ -1,3 +1,35 @@
 document.getElementById("back").addEventListener("click", () => {
     window.location.href = "index.html";
 });
+
+document.getElementById("signUpBtn").addEventListener("click", () => {
+    let xttp = new XMLHttpRequest();
+    const url = "";
+    xttp.open("POST",url,true);
+    xttp.send();
+
+    xttp.onreadystatechange = () => {
+        if (this.readyState === 4 && this.status) {
+            showStats(xttp.responseText);
+        }
+    }
+});
+
+showStats = (json) => {
+    let items = JSON.parse(json);
+    if (items.length !== 0) {
+        items.forEach(data => {
+            let newRow = document.createElement("TR");
+            let text =
+            `<
+                <td>${data["method"]}</td>
+                <td>${data["endpoint"]}</td>
+                <td>${data["count"]}</td>
+            `;
+            newRow.innerHTML = text;
+
+            let table = document.getElementById("statsTable");
+            table.appendChild(newRow);
+        })
+    }
+};
